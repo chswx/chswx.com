f73ac16ab811205d9261f8157942932691ae1a21
Charleston Weather <chswx@jaredwsmith.com>
2012-04-25 05:37:48 +0000

    GA upgrade and domain checks primarily for server switch ID

--- index.php
+++ index.php
@@ -322,14 +322,20 @@ if($conditions != 0) { ?>
 <div id="footer"><div id="footer_wrapper">Follow Charleston Weather updates on <a href="http://twitter.com/chswx">Twitter</a> and <a href="http://identi.ca/chswx">identi.ca</a> / Data by <a href="http://www.wunderground.com/US/SC/Charleston.html">Weather Underground</a><br /><br /><strong>Disclaimer:</strong> Use this page at your own risk. Not intended for use for life-or-death decisions. Refer to official statements from the National Weather Service/local emergency management in case of severe weather.</div>
 </div>
 <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-438397-7']);
+  _gaq.push(['_setDomainName', '<?php echo $_SERVER['HTTP_HOST']?>);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 </script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-438397-7");
-pageTracker._setDomainName(".charlestonwx.com");
-pageTracker._trackPageview();
-} catch(err) {}</script>
 </body>
+<!-- Served by <?php echo $_SERVER['HTTP_HOST']; ?> -->
 </html>
