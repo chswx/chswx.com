7128d8b3a0225b8439d130a5a9431d42d8356246
Jared Smith <jared@jaredwsmith.com>
2015-11-24 23:06:06 -0500

    Add favicon refs

--- index.php
+++ index.php
@@ -9,7 +9,26 @@ $temperature = $data['current_observation']['temp_f'];
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>Currently <?php if(isset($data)) { echo $temperature ?> / <?php echo $data['current_observation']['weather']; } ?> | Charleston, SC Weather</title>
-<link rel="icon" href="images/favicon.png" type="image/png" />
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png?v=A00YePnb9k">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png?v=A00YePnb9k">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png?v=A00YePnb9k">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png?v=A00YePnb9k">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png?v=A00YePnb9k">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png?v=A00YePnb9k">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png?v=A00YePnb9k">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png?v=A00YePnb9k">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png?v=A00YePnb9k">
+<link rel="icon" type="image/png" href="/favicon-32x32.png?v=A00YePnb9k" sizes="32x32">
+<link rel="icon" type="image/png" href="/android-chrome-192x192.png?v=A00YePnb9k" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-96x96.png?v=A00YePnb9k" sizes="96x96">
+<link rel="icon" type="image/png" href="/favicon-16x16.png?v=A00YePnb9k" sizes="16x16">
+<link rel="manifest" href="/manifest.json?v=A00YePnb9k">
+<link rel="shortcut icon" href="/favicon.ico?v=A00YePnb9k">
+<meta name="apple-mobile-web-app-title" content="#chswx">
+<meta name="application-name" content="#chswx">
+<meta name="msapplication-TileColor" content="#2b5797">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png?v=A00YePnb9k">
+<meta name="theme-color" content="#ffffff">
 <style type="text/css">
 html
 {
