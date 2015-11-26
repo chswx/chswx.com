913ebe39193460d3d369570706cf831d3cbbc460
Jared Smith <jared@jaredwsmith.com>
2015-11-26 00:50:39 -0500

    Add Google Analytics include, first rev of template

--- index.php
+++ index.php
@@ -31,22 +31,11 @@ $temperature = $data['current_observation']['temp_f'];
 <style type="text/css">
 <?php include('inc/css/style.css'); ?>
 </style>
-<?php if(GOOGLE_ANALYTICS): ?>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-438397-7']);
-  _gaq.push(['_setAllowLinker', true]);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-<?php endif; ?>
+<?php 
+if(GOOGLE_ANALYTICS) {
+	include('inc/ga.php');
+}
+?>
 <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 <script>
 $(document).ready(function($) {
