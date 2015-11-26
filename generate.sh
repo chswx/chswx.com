d104386873a59a5e347b15d65694eb651804aecd
Jared Smith <jared@jaredwsmith.com>
2015-11-26 01:46:00 -0500

    Make generate.sh use environment variables, set paths, etc.

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget --quiet -P /home/chswx/www/data/ -N http://api.wunderground.com/api/$WU_API_KEY/alerts/conditions/forecast/bestfct:0/q/KCHS.json > /dev/null;
+cd $CHSWX_PATH
+wget --quiet -P /home/chswx/www/data/ -N http://api.wunderground.com/api/$WU_API_KEY/alerts/conditions/forecast/bestfct:0/q/KCHS.json > /dev/null
 /usr/bin/php /home/chswx/www/index.php > /home/chswx/www/index.html
