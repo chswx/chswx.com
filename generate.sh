21fab137727ebab749283e0e5f94e53f70c38d5e
Jared Smith <jared@jaredwsmith.com>
2018-11-10 10:13:22 -0500

    No more NWS data from Wunderground :(

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $CHSWX_PATH
-wget --quiet -P ./data/ -N http://api.wunderground.com/api/$WU_API_KEY/alerts/conditions/forecast/bestfct:0/q/KCHS.json > /dev/null
+wget --quiet -P ./data/ -N http://api.wunderground.com/api/$WU_API_KEY/alerts/conditions/forecast/q/KCHS.json > /dev/null
 /usr/bin/php index.php > index.html
