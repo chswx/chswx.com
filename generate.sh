ad74dd38cf7071ed3994b81bcb5f6979b88e9b44
Jared Smith <jared@jaredwsmith.com>
2018-12-18 17:26:34 -0500

    Update generation script to copy active JSON data to WP

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd $CHSWX_PATH
 wget --quiet -P ./data/ -N http://api.wunderground.com/api/$WU_API_KEY/alerts/conditions/forecast/q/KCHS.json > /dev/null
+cp ./data/KCHS.json /home/chswx/wp/wp-content/uploads/
 /usr/bin/php index.php > index.html
